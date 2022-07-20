398befef1a3ae8a5c06ab5d84353a3d3c5418423
shivang <shivang_trivedi@codenscious.ai>
2022-07-20 15:25:03 +0530

    Tupple SUm Class

--- array basics/merge_sorted.py
+++ array basics/merge_sorted.py
@@ -1,27 +1,50 @@
 a = [3, 6, 8, 12, 45, 321]
 b = [1, 3, 4, 9, 11]
-len_c = len(a) + len(b)
-c = [None] * len_c
 
-i, j, k = 0, 0, 0
+class Merge:
+    def __init__(self, list_1, list_2):
+        self.a = list_1
+        self.b = list_2
+        self.c = [None] * (len(self.a) + len(self.b))
 
-while i < len(a) and j < len(b):
-    if a[i] < b[j]:
-        c[k] = a[i]
-        i += 1
-    else:
-        c[k] = b[j]
-        j += 1
-    k += 1
+        i, j, k = 0, 0, 0
 
-while i < len(a):
-    c[k] = a[i]
-    i += 1
-    k += 1
+        while i < len(self.a) and j < len(self.b):
+            if self.a[i] < self.b[j]:
+                self.c[k] = self.a[i]
+                i += 1
+            else:
+                self.c[k] = self.b[j]
+                j += 1
+            k += 1
 
-while j < len(b):
-    c[k] = b[j]
-    j += 1
-    k += 1
+        while i < len(a):
+            self.c[k] = a[i]
+            i += 1
+            k += 1
 
-print(c)
+        while j < len(b):
+            self.c[k] = b[j]
+            j += 1
+            k += 1
+
+        return self.c
+        
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ array basics/tupple_sum.py
@@ -0,0 +1,68 @@
+
+class TuppleSum:
+
+    def __init__(self, a):
+        self.a = a
+        self.n = len(a)
+
+    def by_exhaustive_search(self, sum):
+        b = []
+
+        for i in range(self.n):
+            for j in range(i + 1, self.n):
+                if self.a[i] + self.a[j] == sum:
+                    b.append((self.a[i], self.a[j]))
+                    break
+        return b
+
+    def by_sorting(self, sum):
+        a = self.a.copy()
+
+        a.sort()
+        b = []
+
+        i, j = 0, self.n - 1
+
+        while i < j:
+            if a[i] + a[j] < sum:
+                i += 1
+            elif a[i] + a[j] > sum:
+                j -= 1
+            else:
+                b.append((a[i], a[j]))
+                i += 1
+                j -= 1
+
+        return b
+
+
+    def by_dictionary(self, sum):
+        # init a dictionary 
+        d = {}
+
+        # to hold the list of touples
+        b = []
+
+        for i in range(self.n):
+            d[self.a[i]] = True
+
+        for i in range(self.n):
+            key = sum - self.a[i]
+            if key in d and d[key] == True:
+                b.append((self.a[i], key))
+                d[self.a[i]] = False
+
+        return b
+
+
+
+x = [3, 1, 2, 8, 4, 6, 7]
+
+tupples = TuppleSum(x)
+
+print(tupples.a)
+
+# print(tupples.by_exhaustive_search(sum = 10))
+# print(tupples.by_sorting(sum = 10))
+print(tupples.by_dictionary(sum = 10))
+print(tupples.a)
