c0324dc361f4a8ea1245271aaa6f4d7e47b34f96
shivang <shivang_trivedi@codenscious.ai>
2022-07-22 15:07:24 +0530

    sotring started

---
+++ sorting/sort.py
@@ -0,0 +1,42 @@
+# 22 July 2022
+
+class Sort:
+    def __init__(self, arr):
+        self.arr = arr
+
+    def bubble(self):
+        arr = self.arr.copy()
+        n = len(arr) - 1
+
+        for j in range(n):
+            for i in range(n - j):
+                if arr[i] > arr[i + 1]:
+                    temp = arr[i]
+                    arr[i] = arr[i + 1]
+                    arr[i + 1] = temp   
+        
+        return arr
+
+    def selection(self):
+        pass
+
+    def insertion(self):
+        pass
+
+    def merge(self):
+        pass
+
+    def quick(self):
+        pass
+
+    def radix(self):
+        pass
+
+
+a = [13, 3, 6, 0, -1, 50, 20, 31]
+
+
+s = Sort(arr = a)
+sorted_arr = s.bubble()
+
+print(sorted_arr)
